0f1fbf6cc397d2ee6bb838f339cfcaa6b5e6fa7d
Nick Wang <nicholas.wang.h@gmail.com>
2019-06-22 12:46:24 +1000

    Refactored simple functions using arrow notation

--- script.js
+++ script.js
@@ -13,13 +13,9 @@ for (let i = 0; i < divs.length; i++) {
 	cards[i] = new Card(i, divs[i], colours[i]);
 
 	// show
-	setTimeout(function(){
-		cards[i].flip();
-	}, 500);
+	setTimeout(() => cards[i].flip(), 500);
 	// hide
-	setTimeout(function(){
-		cards[i].flip();
-	}, 2500);
+	setTimeout(() => cards[i].flip(), 2500);
 
 	// click event
 	divs[i].addEventListener("click", function(){
